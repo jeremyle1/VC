e37b6c878e5431e5331c7998ca0e085e48143c83
Jeremy Le <jeremy_le1@yahoo.com>
2018-08-13 00:13:45 -0500

    implemented double_straight method, and added some tests

--- source/Rules.py
+++ source/Rules.py
@@ -18,11 +18,25 @@ def straight(cards):
     ranks = [card.rank for card in sorted(cards, key=Card.hearts_high)]
 
     try:
+        # Get slice indices. A straight will have ranks that are a sub-sequence of all_ranks.
         first_rank = all_ranks.index(ranks[0])
         last_rank = all_ranks.index(ranks[-1])
         return 2 < len(cards) < 13 and all_ranks[first_rank:last_rank + 1] == ranks
+    except IndexError:
+        return False
     except ValueError:
         return False
 
 def double_straight(cards):
     """Returns true if cards contains 3 or more pairs, with each pair being one rank higher than the previous."""
+    if len(cards) % 2 == 1 or len(cards) < 6 or len(cards) > 24:
+        return False
+    cards_copy = sorted(cards, key=Card.hearts_high)
+
+    # Return False if a pair of cards have differing ranks.
+    for i in range(0, len(cards), 2):
+        if not double([cards_copy[i], cards_copy[i+1]]):
+            return False
+
+    # Return True if the ranks of the doubles make a straight.
+    return straight(cards_copy[::2])
--- tests/test_rules.py
+++ tests/test_rules.py
@@ -1,52 +1,84 @@
 import unittest
 from source.Card import Card
 from source import Rules
+import random
 
 ranks = [str(n) for n in range(3, 11)] + list('JQKA2')
+suits = ['spades', 'clubs', 'diamonds', 'hearts']
 class TestRules(unittest.TestCase):
     def test_double(self):
+        self.assertEqual(Rules.double([]), False)
         cards1 = [Card('3', 'spades'), Card('3', 'hearts')]
-        cards2 = [Card('A', 'spades'), Card('A', 'spades')]
-        cards3 = [Card('3', 'spades'), Card('2', 'hearts')]
-        cards4 = [Card('2', 'spades'), Card('2', 'spades'), Card('2', 'spades')]
-        cards6 = [Card(2, 'spades')]
         self.assertEqual(Rules.double(cards1), True)
+        cards2 = [Card('A', 'spades'), Card('A', 'spades')]
         self.assertEqual(Rules.double(cards2), True)
+        cards3 = [Card('3', 'spades'), Card('2', 'hearts')]
         self.assertEqual(Rules.double(cards3), False)
+        cards4 = [Card('2', 'spades'), Card('2', 'spades'), Card('2', 'spades')]
         self.assertEqual(Rules.double(cards4), False)
+        cards6 = [Card(2, 'spades')]
         self.assertEqual(Rules.double(cards6), False)
 
     def test_triple(self):
+        self.assertEqual(Rules.triple([]), False)
         cards1 = [Card('2', 'spades'), Card('2', 'spades'), Card('2', 'spades')]
-        cards2 = [Card('2', 'spades'), Card('2', 'hearts'), Card('2', 'diamonds')]
-        cards3 = [Card('2', 'spades'), Card('2', 'spades'), Card('3', 'spades')]
         self.assertEqual(Rules.triple(cards1), True)
+        cards2 = [Card('2', 'spades'), Card('2', 'hearts'), Card('2', 'diamonds')]
         self.assertEqual(Rules.triple(cards2), True)
+        cards3 = [Card('2', 'spades'), Card('2', 'spades'), Card('3', 'spades')]
         self.assertEqual(Rules.triple(cards3), False)
 
     def test_quad(self):
+        self.assertEqual(Rules.quad([]), False)
         cards1 = [Card('2', 'spades'), Card('2', 'spades'), Card('2', 'spades'), Card('2', 'spades')]
-        cards2 = [Card('2', 'clubs'), Card('2', 'spades'), Card('2', 'diamonds'), Card('2', 'hearts')]
-        cards3 = [Card('2', 'clubs'), Card('2', 'spades'), Card('2', 'diamonds'), Card('A', 'hearts')]
         self.assertEqual(Rules.quad(cards1), True)
+        cards2 = [Card('2', 'clubs'), Card('2', 'spades'), Card('2', 'diamonds'), Card('2', 'hearts')]
         self.assertEqual(Rules.quad(cards2), True)
+        cards3 = [Card('2', 'clubs'), Card('2', 'spades'), Card('2', 'diamonds'), Card('A', 'hearts')]
         self.assertEqual(Rules.quad(cards3), False)
 
     def test_straight(self):
+        self.assertEqual(Rules.straight([]), False)
         cards1 = [Card(rank, 'spades') for rank in ranks[0:12]]
-        cards2 = [Card(rank, 'spades') for rank in ranks]
-        cards3 = [Card(rank, 'spades') for rank in ranks[0:2]]
-        cards4 = [Card(rank, 'spades') for rank in ranks[0:3]]
-        cards5 = cards1[::-1]
-        cards6 = cards2[::-1]
-        cards7 = cards4[::-1]
         self.assertEqual(Rules.straight(cards1), True)
+        cards2 = [Card(rank, 'spades') for rank in ranks]
         self.assertEqual(Rules.straight(cards2), False)
+        cards3 = [Card(rank, 'spades') for rank in ranks[0:2]]
         self.assertEqual(Rules.straight(cards3), False)
+        cards4 = [Card(rank, 'spades') for rank in ranks[0:3]]
         self.assertEqual(Rules.straight(cards4), True)
+        cards5 = cards1[::-1]
         self.assertEqual(Rules.straight(cards5), True)
+        cards6 = cards2[::-1]
         self.assertEqual(Rules.straight(cards6), False)
+        cards7 = cards4[::-1]
         self.assertEqual(Rules.straight(cards7), True)
+        cards8 = [Card(rank, 'spades') for rank in ranks[10:13]]
+        self.assertEqual(Rules.straight(cards8), False)
+        cards9 = [Card(rank, 'spades') for rank in ranks[9:12]]
+        self.assertEqual(Rules.straight(cards9), True)
+
+    def test_double_straight(self):
+        self.assertEqual(Rules.double_straight([]), False)
+        cards1 = [Card(rank, random.choice(suits)) for rank in [3, 3, 4, 4, 5, 5]]
+        self.assertEqual(Rules.double_straight(cards1), True)
+        cards2 = [Card(rank, random.choice(suits)) for rank in [3, 3, 4, 5, 5, 5]]
+        self.assertEqual(Rules.double_straight(cards2), False)
+        cards3 = [Card(rank, random.choice(suits)) for rank in [3, 3, 4, 4, 5, 5, 6, 6]]
+        self.assertEqual(Rules.double_straight(cards3), True)
+        cards4 = [Card(rank, random.choice(suits)) for rank in [3, 3, 4, 4]]
+        self.assertEqual(Rules.double_straight(cards4), False)
+        cards5 = [Card(rank, random.choice(suits)) for rank in [3, 3]]
+        self.assertEqual(Rules.double_straight(cards5), False)
+        cards6 = [Card(rank, random.choice(suits)) for rank in [3, 3, 4, 4, 5, 5, 6, 6, 8, 8]]
+        self.assertEqual(Rules.double_straight(cards6), False)
+        cards7 = [Card(rank, random.choice(suits)) for rank in ['Q', 'Q', 'K', 'K', 'A', 'A']]
+        self.assertEqual(Rules.double_straight(cards7), True)
+        cards8 = [Card(rank, random.choice(suits)) for rank in ['K', 'K', 'A', 'A', 2, 2]]
+        self.assertEqual(Rules.double_straight(cards8), False)
+        cards9 = [Card(rank, random.choice(suits)) for rank in [3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10,
+                                                                'J', 'J', 'Q', 'Q', 'K', 'K', 'A', 'A']]
+        self.assertEqual(Rules.double_straight(cards9), True)
 
 if __name__ == '__main__':
     unittest.main()
